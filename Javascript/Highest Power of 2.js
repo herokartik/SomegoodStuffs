2b472e0ee051b07b0c081df58b626866774cf18c
Kartikeswar Rana <98409611+herokartik@users.noreply.github.com>
2022-06-16 19:59:01 +0530

    Update Highest Power of 2.js

--- Javascript/Highest Power of 2.js
+++ Javascript/Highest Power of 2.js
@@ -1,3 +1,6 @@
+/* Here we are using inbult log function for Finding the highest
+power of 2 equal or less then the number*/
+
 function power2(n){
     let p=parseInt(Math.log(n)/Math.log(2),10)
     return Math.pow(2,p)
